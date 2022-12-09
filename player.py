6662ade9ae1aa5551bef88a48868fdc29db5ca56
Spinny2005 <spencer_boggs@icloud.com>
2022-12-09 15:03:26 -0800

    File Location Update

--- player.py
+++ player.py
@@ -1,12 +1,20 @@
 import pygame
 from settings import *
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('/home/spencer/PythonAdventure/graphics/test/player.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect.inflate(0,-26)
+        try:
+            self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
+        except FileNotFoundError:
+            try:
+                self.image = pygame.image.load('/home/spencer/PythonAdventure/graphics/test/player.png').convert_alpha()
+            except FileNotFoundError:
+                self.image = pygame.image.load('$HOME/PythonAdventure/graphics/test/player.png').convert_alpha()
+
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -26)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
@@ -29,11 +37,11 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 1
         else:
             self.direction.x = 0
-    
+
     def move(self, speed):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
-            
+
         self.hitbox.x += self.direction.x * speed
         self.collision('horizontal')
         self.hitbox.y += self.direction.y * speed
--- tile.py
+++ tile.py
@@ -1,9 +1,17 @@
 import pygame
 from settings import *
 
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('/home/spencer/PythonAdventure/graphics/test/rock.png').convert_alpha()
+        try:
+            self.image = pygame.image.load('./graphics/test/rock.png').convert_alpha()
+        except FileNotFoundError:
+            try:
+                self.image = pygame.image.load('/home/spencer/PythonAdventure/graphics/test/rock.png').convert_alpha()
+            except FileNotFoundError:
+                self.image = pygame.image.load('$HOME/PythonAdventure/graphics/test/rock.png').convert_alpha()
+
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
